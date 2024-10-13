633ceeee4fa0cf4a719de56a7f4c372a00d1a98d
Arseniy <cenz1217@gmail.com>
2024-10-13 18:01:30 +0500

    added path to file, GUI python image

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,19 @@
-FROM python:3.9-slim
+# Используем базовый образ с поддержкой графики
+FROM jupyter/scipy-notebook:latest
 
-RUN apt-get update && \
-    apt-get install -y build-essential \
-    && apt-get clean
+# RUN apt-get update && \
+#     apt-get install -y build-essential \
+#     && apt-get clean
 
 
 
-RUN pip install --no-cache-dir opencv-python-headless   
+RUN pip install --no-cache-dir opencv-python-headless opencv-python
 
 COPY face_detection.py /app/face_detection.py
 COPY photo_1.jpg /app/photo_1.jpg
 
 WORKDIR /app
+
+ENV DISPLAY=:0
 
 CMD ["python", "face_detection.py"]
--- face_detection.py
+++ face_detection.py
@@ -1,6 +1,19 @@
 import cv2
+import sys
+import argparse
 
-image = cv2.imread('photo_1.jpg')
+parser = argparse.ArgumentParser(description="Face detection on an image.")
+parser.add_argument('image_path', nargs='?', default='photo_1.jpg', help='Path to the image file')
+
+args = parser.parse_args()
+image_path = args.image_path
+
+image = cv2.imread(image_path)
+
+if image is None:
+    print(f"Error: Image '{image_path}' not found.")
+    sys.exit(1)
+
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -9,7 +22,8 @@ faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, min
 print(f"Found {len(faces)} face(s)")
 
 for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-# cv2.imshow("Faces", image)
-# cv2.waitKey(0)
+cv2.imshow('Detected Faces', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
